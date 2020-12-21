90b6a564dcad426437b2143c53169d0c6410e571
Bruno Faugeroux <clowrid@gmail.com>
2020-12-21 09:39:25 +0100

    Update watch list

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import Societe from './Societe.js';
 
 $(document).ready(async function() {
 
-    let societeCodes = ['AURW','PFP','PAMUN','PFLY','PCARM','PGFC','PIMDA','PEN','PKORI'];
+    let societeCodes = ['AURW','PFP','PAMUN','PFLY','PCARM','PGFC','PIMDA','PEN','PKORI','APRX'];
     let listeSociete = [];
     let listePromesse = [];
 
